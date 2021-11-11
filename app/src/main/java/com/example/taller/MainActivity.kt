471f7c5dd4a9f6175d27761fa7049a671c25f4c3
jecueva1997 <reatiguijeferson@gmail.com>
2021-11-10 23:14:31 -0500

    Segunda parte del taller

--- app/src/main/java/com/example/taller/MainActivity.kt
+++ app/src/main/java/com/example/taller/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
+            Taller primera parte
+         */
         // Validar si una persona es mayor o menor de edad
         // mayorEdad()
 
@@ -15,11 +18,24 @@ class MainActivity : AppCompatActivity() {
         // tablaMultiplicar()
 
         // Mostrar el listado de paralelo de la materia de Plataformas móviles y los subgrupos por proyectos
-        proyectos()
+        // proyectos()
 
         // propiedades de un vehículo
         // clase()
+
+        /*
+            Taller segunda parte
+         */
+        // Ordenas los números
+        // orden()
+
+        // Validar la cédula
+        validarCedula()
+
     }
+    /*
+        Taller primera parte
+     */
 
     private fun mayorEdad(){
         val edad = 60
@@ -45,7 +61,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun proyectos(){
-        
+        var x = 4
+        var y = 5
+        val estudiantes= arrayListOf<String>()
+        val primerGrupo= arrayListOf<String>()
+        val segundoGrupo= arrayListOf<String>()
+        estudiantes.addAll(listOf("Joan", "Jeferson", "Jordy", "David", "Frank", "Shomira", "Luis",
+            "Ricardo", "Andrés"))
+
+        while (x >= 0){
+            primerGrupo.add(estudiantes[x])
+            x-=1
+        }
+        println("Primer Grupo: ${primerGrupo.sorted()}")
+
+        while (y < 10){
+            segundoGrupo.add(estudiantes[y])
+            y+=1
+        }
+        println(" Segundo Grupo: ${segundoGrupo.sorted()}")
     }
 
     private fun clase(){
@@ -56,5 +90,47 @@ class MainActivity : AppCompatActivity() {
         vehiculo.tipoMotor()
         vehiculo.tipoTraccion()
         vehiculo.tipoCapacidad()
+    }
+
+    /*
+        Taller segunda parte
+     */
+    private fun orden(){
+
+        val numeros = intArrayOf(8, 3, 4, 1, 2, 6, 5, 7)
+
+        for(x in  (0 until numeros.size-1)) {
+            for(y in (0 until numeros.size-1)) {
+                if(numeros[y] < numeros[y+1]) {
+                    val tmp = numeros[y]
+                    numeros[y] = numeros[y+1]
+                    numeros[y+1] = tmp
+                }
+            }
+        }
+        for (x in numeros){
+            println(x)
+        }
+    }
+
+    private fun validarCedula(){
+        var cedula = intArrayOf(1,1,5,0,0,6,5,3,9,7)
+        var validar = intArrayOf(2,1,2,1,2,1,2,1,2)
+        var cont = 0
+        var suma = 0
+        for (x in validar){
+            var res = x * cedula[cont]
+            cont += 1
+            if (res > 9){
+                var aux1 = res.toString()
+                res = Character.getNumericValue(aux1.get(0)) + Character.getNumericValue(aux1.get(1))
+            }
+            suma += res
+        }
+        if (cedula[9] == (suma % 10) || cedula[9] == 10 - (suma % 10)){
+            println("La cédula  es correcta")
+        }else{
+            println("La cédula no es correcta")
+        }
     }
 }
